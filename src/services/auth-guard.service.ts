27103ca117d1c0fa0eff380852afbfbb8f0e47bf
zelmad <zelmadmohamed1994@gmail.com>
2019-11-27 19:56:16 +0100

    Adding-redirect-component

---
+++ src/services/auth-guard.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthServiceService} from './auth-service.service';
+
+@Injectable()
+export class AuthGuardService implements CanActivate {
+
+  constructor(private authService: AuthServiceService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isAuth) {
+      return true;
+    } else {
+      this.router.navigate(['/auth']);
+    }
+  }
+}
